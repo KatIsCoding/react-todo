b43bb8f6d99d88aceb73687a9f9c45ef325d72ee
KatIsCoding <fabrigoru@gmail.com>
2021-10-25 16:50:14 -0600

    Basic Styling

--- src/components/newItem.js
+++ src/components/newItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.css';
 
 class NewTodoForm extends React.Component {
   constructor(props) {
@@ -11,18 +12,22 @@ class NewTodoForm extends React.Component {
     const { newTodoEventHandler } = this.props;
     const { title } = this.state;
     return (
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        if (title.trim()) {
-          e.target[0].value = '';
-          newTodoEventHandler(this.state);
-          this.setState({ title: '' });
-        }
-      }}
-      >
-        <input onKeyUp={(e) => {
-          this.setState({ title: e.target.value });
+      <form
+        className="NewTodoForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (title.trim()) {
+            e.target[0].value = '';
+            newTodoEventHandler(this.state);
+            this.setState({ title: '' });
+          }
         }}
+      >
+        <input
+          placeholder="Add New ToDo..."
+          onKeyUp={(e) => {
+            this.setState({ title: e.target.value });
+          }}
         />
         <button type="submit">Submit</button>
       </form>

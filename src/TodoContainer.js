0a567d84e6de815f7901904377b7fdbea9ae3613
KatIsCoding <fabrigoru@gmail.com>
2021-10-25 13:47:15 -0600

    Refactor to function component

--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -2,26 +2,10 @@ import './App.css';
 import React from 'react';
 import TodoList from './components/todoList';
 
-class TodoContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-        {
-          id: 1,
-          title: 'Test',
-          completed: false,
-        },
-      ],
-    };
-  }
-
-  render() {
-    const { todos } = this.state;
-    return (
-      <TodoList todos={todos} />
-    );
-  }
+function TodoContainer() {
+  return (
+    <TodoList />
+  );
 }
 
 export default TodoContainer;
